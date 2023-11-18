58ece167d23a06d545758d3aa5041f51da037b21
3steban99 <barrionuevo99@outlook.com>
2023-11-18 16:37:48 -0300

    ejercicio del challenge 7

---
+++ challenge-7/areaCirculo/circulo.js
@@ -0,0 +1,7 @@
+let radio = parseInt(prompt("ingrese el radio para calcular el area"))
+
+let areaResultado = 3.14 * (radio**2)
+
+console.log (areaResultado)
+
+document.write("El resultado es igual a :", areaResultado + "m2")
---
+++ challenge-7/areaTriangulo/triangulo.js
@@ -0,0 +1,9 @@
+let base = parseInt(prompt("ingrese el valor de la base del triangulo"))
+
+let altura = parseInt(prompt("ingrese la altura del triangulo"))
+
+let area = (base*altura)/2
+
+console.log(area)
+
+document.write("El Resultado es igual a: ", area)
