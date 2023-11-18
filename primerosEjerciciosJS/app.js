2f20dd7fcd2cf8be257fb3cb35ed806a8f4d42dd
3steban99 <barrionuevo99@outlook.com>
2023-11-18 20:36:59 -0300

    primeros ejercicios de js

---
+++ primerosEjerciciosJS/app.js
@@ -0,0 +1,124 @@
+//Aqui van a estar todos los ejercicios del Documento
+
+//Ejercicio 1:
+
+    //alert("hola mundo")
+
+//Ejercicio 2:
+
+    //document.write("Heelo World")
+
+//Ejercicio 3:
+
+    // let sumar = 3+5;
+
+    // document.write("El resultado de sumar 3 + 5 es igual a: ", sumar)
+
+//Ejercicio 4
+
+    // let nombre = prompt("Ingrese su nombre")
+
+    // document.write("Hola ", nombre)
+
+//Ejercicio 5
+
+    // let num1 = parseInt(prompt("Ingrese el primer numero para sumar"))
+    // let num2 = parseInt(prompt("Ingrese el segundo numero"))
+
+    // document.write("La suma es igual a: ",(num1+num2))
+
+//Ejercicio 6
+
+    // let num1 = parseInt(prompt("Ingrese el primer numero"))
+    // let num2 = parseInt(prompt("Ingrese el segundo numero"))
+
+    // let mayor = Math.max(num1, num2)
+
+    // document.write("El numero mayor es: ",mayor)
+
+//Ejercicio 7
+
+    // let num1 = parseInt(prompt("Ingrese el primer numero"))
+    // let num2 = parseInt(prompt("Ingrese el segundo numero"))
+    // let num3 = parseInt(prompt("Ingrese el tercer numero"))
+
+    // let mayor = Math.max(num1, num2, num3)
+
+    // document.write("El numero mayor es: ",mayor)
+
+//Ejercicio 8
+
+    // let num = parseInt(prompt("ingrese un numero"))
+
+    // if(num % 2 === 0){
+    //     document.write("el numero "+num+" es divisible por 2")
+    // }else{
+    //     document.write("el numero "+num+" no es divisible por 2")
+    // }
+
+//Ejercicio 9
+
+    // let palabra = prompt("Ingresa una palabra:").toLowerCase();
+
+    // let palabraSinVocales = "";
+    // for (let i = 0; i < palabra.length; i++) {
+    //     let letra = palabra.charAt(i);
+    //     if ("aeiou".includes(letra)) {
+    //         palabraSinVocales += letra;
+    //     }
+    // }
+
+    // if (palabraSinVocales !== "") {
+    //     console.log("Las vocales son: " + palabraSinVocales);
+    //     document.write("Las vocales de la palabra ingresa son : ", palabraSinVocales);
+    // } else {
+    //     console.log("La palabra no contiene vocales.");
+    // }
+
+//Ejercicio 10
+
+    // let numA = parseInt(prompt("Ingrese un numero"))
+
+    // if(numA % 2 === 0){
+    //     document.write("El "+numA+" es divisible por 2")
+    // }
+    // else if(numA % 3 === 0){
+    //     document.write("El "+numA+" es divisible por 3")
+    // }
+    // else if(numA % 5 === 0){
+    //     document.write("El "+numA+" es divisible por 5")
+    // }
+    // else if(numA % 7 === 0){
+    //     document.write("El "+numA+" es divisible por 7")
+    // }
+    // else{
+    //     document.write("El "+numA+" no es divisible por 2, 3, 5 o 7")
+    // }
+
+//Ejercicio 11
+
+    // let numero = parseInt(prompt('Ingresa un número:'));
+    // let divisibles = '';
+
+    // if (numero % 2 === 0) {
+    //     divisibles += '2 y ';
+    // }
+    // if (numero % 3 === 0) {
+    //     divisibles += '3 y ';
+    // }
+    // if (numero % 5 === 0) {
+    //     divisibles += '5 y ';
+    // }
+    // if (numero % 7 === 0) {
+    //     divisibles += '7 y ';
+    // }
+
+    // if (divisibles !== '') {
+    
+    //     divisibles = divisibles.slice(0, -2);
+    //     console.log(`El ${numero} es divisible por ${divisibles}.`);
+    //     document.write(`El ${numero} es divisible por ${divisibles}.`)
+    // } else {
+    //     console.log(`El ${numero} no es divisible por 2, 3, 5 ni 7.`);
+    //     document.write(`El ${numero} no es divisible por 2, 3, 5 ni 7.`)
+    // }
